f2f4ff21fc4fdc2379d4357a64c27fd0c2bcbc1b
Mohamed Emam <m.emam9595@gmail.com>
2018-03-22 20:58:00 +0200

    SGMath Common Header

---
+++ SGMath/sgmath.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 Mohamed Emam
+ * The code is licensed under the MIT License.
+ * You can check the file LICENSE for the full license.
+ *
+ * This is a common header for all SGMath headers.
+ */
+
+#ifndef SGMATH_H
+#define SGMATH_H
+
+#include "constants.h"
+//#include "funcs.h"
+//#include "matrix3.h"
+#include "matrix4.h"
+//#include "matrixn.h"
+#include "normal.h"
+#include "point2.h"
+#include "point3.h"
+#include "ray.h"
+//#include "transformations.h"
+//#include "vector2.h"
+#include "vector3.h"
+//#include "vector4.h"
+//#include "vectorn.h"
+
+#endif
